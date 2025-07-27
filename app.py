526ac784a04f289ba82f33bb5baa448bfbda14be
Jhonny Lima <lima.jhonny95@gmail.com>
2025-07-27 16:46:20 -0300

    feat: add `get_task` endpoint

--- app.py
+++ app.py
@@ -8,19 +8,26 @@ task_id_counter = 1
 
 @app.route('/tasks', methods=['POST'])
 def create_task():
-    global task_id_counter
-    data = request.get_json()
-    new_task = Task(id=task_id_counter, title=data['title'], description=data.get('description', ''))
-    task_id_counter += 1
-    tasks.append(new_task)
-    return jsonify({'message': 'Nova tarefa criada com sucesso'})
+  global task_id_counter
+  data = request.get_json()
+  new_task = Task(id=task_id_counter, title=data['title'], description=data.get('description', ''))
+  task_id_counter += 1
+  tasks.append(new_task)
+  return jsonify({'message': 'Nova tarefa criada com sucesso'})
 
 @app.route('/tasks', methods=['GET'])
 def get_tasks():
-    return jsonify({
-        'tasks': [task.to_dict() for task in tasks],
-        'total_tasks': len(tasks)
-    })
+  return jsonify({
+    'tasks': [task.to_dict() for task in tasks],
+    'total_tasks': len(tasks)
+  })
+
+@app.route('/tasks/<int:id>', methods=['GET'])
+def get_task(id):
+  for task in tasks:
+    if task.id == id:
+      return jsonify(task.to_dict())
+  return jsonify({'message': 'Tarefa não encontrada'}), 404
 
 if __name__ == '__main__':
-    app.run(debug=True)
+  app.run(debug=True)
